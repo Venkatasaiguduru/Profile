19ee92efc701c450a67a4e33d4405157e211a45b
2305A21L54 <venkatasaiguduru@gmail.com>
2025-07-07 11:27:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from PIL import Image
 st.sidebar.title("About")
 # st.sidebar.markdown("## Sections")
 with st.sidebar:
-    selection=st.radio("Details", ["About Me", "Skills", "Projects & internships","Certifications & Licenses","Contact Information"])
+    selection=st.radio("Details", ["Professional Summary", "Skills", "Projects & internships","Certifications & Licenses","Contact Information"])
 # side = ["About Me", "Skills", "Projects", "Contact Information"]
 # selection = st.sidebar.radio("Details",side)
 
@@ -20,19 +20,20 @@ width, height = image.size
 st.image(image, width=300)
 st.title("GUDURU VENKATASAI")
 
-if selection == "About Me":
-    st.header("About Me")
-    st.text("Dedicated Electrical and Electronics Engineering, excelling in both core engineering & software development. With a solid foundation in core engineering and a proficient in python programming & basic java. passionate, adaptable, and always eager to learn and contribute to impactful project.")
+if selection == "Professional Summary":
+    st.header("Professional Summary")
+    st.text("Dedicated and passionate Electrical and Electronics Engineering student, excelling in both core engineering and software development. Has a solid foundation in core engineering and programming. Proficient in Python, circuit design, and MATLAB, with practical exposure to substation systems. Known for being adaptable, always eager to learn and driven to contribute to impactful projects..")
     st.markdown("[LinkedIn](https://www.linkedin.com/in/guduru-venkatasai-5aaa69294/)")
     st.markdown("[GitHub](https://github.com/Venkatasaiguduru)")
 
 elif selection == "Skills":
     st.header("Technical Skills")
-    st.text("Programming Languages: Python programming and Basic Java")
+    st.text("Programming Languages: Python programming")
     st.write("Internet of Things")
-    # st.write("Basic Java")
+    st.write("Basic Data structures and algorithms")
     st.write("Basic Artifical intellience")
     st.write("Basic Matlab & Simulation")
+    st.write("Circuit Design in Tinkercad")
     
     st.header("Soft skills")
     st.write("Team work")
@@ -45,10 +46,15 @@ elif selection == "Skills":
 elif selection == "Projects & internships":
     st.header("Projects")
     st.subheader("1.LPG GAS DETECTION AND ACTIVATION OF EXHAUST FAN ")
-    st.text("The project aims to develop an automatic LPG gas leakage detection system that can sense the presence of leaked gas and activate an exhaust fan to vent the gas out, thus reducing the risk of accidents. Additionally, the system should provide alerts to ensure timely awareness and action")
+    st.text("Designed and implemented a system to detect LPG gas leakage and automatically activate an exhaust fan and sent out leaked gas out through chimney for residential safety. Integrated audible and visual alerts using a buzzer and LEDs for immediate notification.Technologies Used: MQ-6 Gas Sensor, ESP 32, Relay Module, Buzzer, LED, Power Supply Module, Breadboard.")
     st.link_button("View project","https://www.linkedin.com/posts/guduru-venkatasai-5aaa69294_innovation-engineering-automation-activity-7267555123470438401-H_3p?utm_source=share&utm_medium=member_desktop")
+    st.subheader("2.Non-Isolated DC-DC Quadratic Voltage Multiplier")
+    st.text("Designed a compact DC-DC converter to boost 24V input to 120V output using a smart topology. Employed a single-switch architecture and a quadratic voltage multiplier circuit with effective use of inductors and capacitors.Achieved high voltage gain with minimal ripple, supporting the shift to clean energy mobility Technologies Used: MOSFET (power switch), Inductors, Diodes, Capacitors, Voltage Multiplier Cell (VMC), PWM control, MATLAB/Simulink.")
+    st.link_button("View project","https://www.linkedin.com/posts/venkatasaiguduru_dcdcconverter-boostconverter-hydrogenvehicles-activity-7327332590850363392-dSVj?utm_source=share&utm_medium=member_desktop&rcm=ACoAAEdZHhIBRVNnCEsWAXyPGVYYq4F7fXWnzW4")
     st.header("Internships")
-    st.subheader("1.ANSYS ELECTROMAGNETIC ANALYSIS VIRTUAL INTERNSHIP! by AICTE,EDUSKILLS")
+    st.subheader("Industrial Training – TS TRANSCO 220/132/33 kV Substation peddur ,Sircilla, Telangana")
+    st.text("Gained hands-on exposure to substation operations, high-voltage equipment, and grid infrastructure. Observed real-time load management and system protection mechanisms in power transmission.Assisted in routine inspections, relay testing, and maintenance procedures.Developed a deeper understanding of power distribution, fault analysis, and switchgear operations.")
+    st.subheader("ANSYS ELECTROMAGNETIC ANALYSIS VIRTUAL INTERNSHIP! by AICTE,EDUSKILLS")
     st.text("Platforms: AICTE National Internship Portal, Eduskills foundation & Ansys.") 
     st.text("Description: The internship focuses on electromagnetic analysis, offering practical knowledge and online courses.Completed a virtual internship focused on electromagnetic analysis using ANSYS software.Earned a course completion certificate and an internship certificate from AICTE and EduSkills")
     st.text("Duration: Typically 2 months")
